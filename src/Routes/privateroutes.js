270d4e43164355b9e808e27bab4724910c859541
Nguyễn Công Minh <88716743+minhNC2000@users.noreply.github.com>
2023-04-18 00:40:38 +0700

    update
    1.0

--- src/Routes/privateroutes.js
+++ src/Routes/privateroutes.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { AuthMiddleware } from '../Middlewares/AuthMiddleware'
+import { Orders } from '../Pages/Orders/Orders'
 
-export const privateroutes = () => {
-  return (
-    <div>privateroutes</div>
-  )
-}
+export const privateroutes = (
+<Route path="don-hang" element = {AuthMiddleware}>
+  <Route path="" element={<Orders />}
+</Route>
+) 
