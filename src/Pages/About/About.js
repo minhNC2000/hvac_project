baa82cbcbcfa7e2182be49f3a508bd43d048e52a
Nguyễn Công Minh <88716743+minhNC2000@users.noreply.github.com>
2023-04-26 00:32:05 +0700

    update UI
    update UI

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -287,7 +287,7 @@ export const About = () => {
           </Grid>
         </Container>
       </section>
-      <div className="counter spad ">
+      <div className="counter spad set-bg">
         <Container className="lg">
           <div className="row">
             <Grid item lg={3} md={6} sm={6} className="counter__wrap">
@@ -314,6 +314,84 @@ export const About = () => {
                 <h2 class="counter-num">5100</h2>
                 <p>Happy Clients</p>
               </div>
+            </Grid>
+          </div>
+        </Container>
+      </div>
+      <div className="client spad">
+        <Container className="lg">
+          <div className="row">
+            <Grid item lg={12}>
+              <div class="section-title">
+                <span>Partner</span>
+                <h2>Our Clients</h2>
+              </div>
+            </Grid>
+          </div>
+          <div className="row">
+            <Grid item lg={3} md={4} sm={6}>
+              <a href="#" class="client__item">
+                <img
+                  src="https://preview.colorlib.com/theme/hvac/img/clients/client-1.png"
+                  alt=""
+                />
+              </a>
+            </Grid>
+            <Grid item lg={3} md={4} sm={6}>
+              <a href="#" class="client__item">
+                <img
+                  src="https://preview.colorlib.com/theme/hvac/img/clients/client-2.png"
+                  alt=""
+                />
+              </a>
+            </Grid>
+            <Grid item lg={3} md={4} sm={6}>
+              <a href="#" class="client__item">
+                <img
+                  src="https://preview.colorlib.com/theme/hvac/img/clients/client-3.png"
+                  alt=""
+                />
+              </a>
+            </Grid>
+            <Grid item lg={3} md={4} sm={6}>
+              <a href="#" class="client__item">
+                <img
+                  src="https://preview.colorlib.com/theme/hvac/img/clients/client-2.png"
+                  alt=""
+                />
+              </a>
+            </Grid>
+            <Grid item lg={3} md={4} sm={6}>
+              <a href="#" class="client__item">
+                <img
+                  src="https://preview.colorlib.com/theme/hvac/img/clients/client-4.png"
+                  alt=""
+                />
+              </a>
+            </Grid>
+            <Grid item lg={3} md={4} sm={6}>
+              <a href="#" class="client__item">
+                <img
+                  src="https://preview.colorlib.com/theme/hvac/img/clients/client-5.png"
+                  alt=""
+                />
+              </a>
+            </Grid>
+            <Grid item lg={3} md={4} sm={6}>
+              <a href="#" class="client__item">
+                <img
+                  src="https://preview.colorlib.com/theme/hvac/img/clients/client-6.png"
+                  alt=""
+                />
+              </a>
+            </Grid>
+            <Grid item lg={3} md={4} sm={6}>
+              <a href="#" class="client__item">
+                <img
+                  src="https://preview.colorlib.com/theme/hvac/img/clients/client-4.png"
+                  alt=""
+                />
+              </a>
             </Grid>
           </div>
         </Container>
--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,5 +1,35 @@
 import React from "react";
+
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+
 import "./blog.scss";
 export const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <>
+      <div className="breadcumb-option">
+        <Container maxWidth="lg">
+          <div className="row">
+            <Grid container spacing={1}>
+              <Grid item lg={12}>
+                <div className="breadcrumb__text">
+                  <h1>Lastest Blogs</h1>
+                  <div className="breadcumb__links">
+                    <a href="/">
+                      <FontAwesomeIcon icon={faHouse} />
+                      Home
+                    </a>
+                    <span>Our Blogs</span>
+                  </div>
+                </div>
+              </Grid>
+            </Grid>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
 };
