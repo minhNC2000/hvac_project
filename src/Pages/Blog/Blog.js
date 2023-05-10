7dc6b8c4d3ec9dab6c8c43568920b24ee15b9bfb
Nguyễn Công Minh <88716743+minhNC2000@users.noreply.github.com>
2023-05-10 16:13:44 +0700

    update UI
    update UI

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -187,7 +187,12 @@ export const Blog = () => {
                     </div>
                   </Grid>
                 </div>
-                <Pagination count={5} color="primary" />
+                <Pagination
+                  count={5}
+                  variant="outlined"
+                  color="secondary"
+                  shape="rounded"
+                />
               </Grid>
               <Grid item lg={3} md={6} sm={9}>
                 <div className="blog__sidebar">
@@ -198,6 +203,70 @@ export const Blog = () => {
                       <FontAwesomeIcon icon={faMagnifyingGlass} />
                     </button>
                   </form>
+                  <div className="blog__sidebar__feature">
+                    <h2>Feature News</h2>
+                    <div className="blog__sidebar__feature__item">
+                      <h4>
+                        <a href="#">Where To Look For Cheap Brochure</a>
+                      </h4>
+                      <ul>
+                        <li>By Polly Williams</li>
+                        <li>Dec 27, 2018</li>
+                      </ul>
+                    </div>
+                    <div className="blog__sidebar__feature__item">
+                      <h4>
+                        <a href="#">Using Banner Stands To Increase</a>
+                      </h4>
+                      <ul>
+                        <li>By Evelyn Lane</li>
+                        <li>Dec 17, 2018</li>
+                      </ul>
+                    </div>
+                    <div className="blog__sidebar__feature__item">
+                      <h4>
+                        <a href="#">Promotional Advertising Specialty</a>
+                      </h4>
+                      <ul>
+                        <li>By Don Townsend</li>
+                        <li>Dec 22, 2018</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="blog__sidebar__categories">
+                    <h2>Categories</h2>
+                    <ul>
+                      <li>
+                        <a href="#">Creativity</a>
+                      </li>
+                      <li>
+                        <a href="#">Design</a>
+                      </li>
+                      <li>
+                        <a href="#">Inspiration</a>
+                      </li>
+                      <li>
+                        <a href="#">News</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="blog__sidebar__tag">
+                    <h2>Tag</h2>
+                    <a href="#">Car Dealer</a>
+                    <a href="#">bmw</a>
+                    <a href="#">Chevrolet</a>
+                    <a href="#">ferrari</a>
+                    <a href="#">Honda</a>
+                    <a href="#">Hatchback</a>
+                  </div>
+                  <div className="blog__sidebar__newslatter">
+                    <h2>Newsletter</h2>
+                    <p>Subscribe our newsletter gor get</p>
+                    <form action="#">
+                      <input type="text" placeholder="Your email" />
+                      <button type="submit">Subscribe</button>
+                    </form>
+                  </div>
                 </div>
               </Grid>
             </Grid>
